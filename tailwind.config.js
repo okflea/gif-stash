03d97db7beec876b0aad660e327446b6065958fa
okflea <god@no.ob>
2023-05-11 22:29:53 +0530

    clearing defaults , bg

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,8 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'hero-pattern': "url('/images/hero-pattern.jpg')",
+        'bg': "url(@/../../public/bg.webp);",
+
         // 'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         // 'gradient-conic':
         //   'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
